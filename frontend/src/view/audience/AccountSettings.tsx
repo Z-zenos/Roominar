c045ae4fd84da8af5d05f08f3ca3c5e4cbad73a7
Z-zenos <hoanganhtuanbk2001@gmail.com>
2024-09-17 17:49:30 +0000

    Add alert

--- frontend/src/view/audience/AccountSettings.tsx
+++ frontend/src/view/audience/AccountSettings.tsx
@@ -1,26 +1,27 @@
 'use client';
 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from '@/src/component/common/Alert';
 import ChangeEmailForm from '@/src/component/form/ChangeEmailForm';
 import ChangePasswordForm from '@/src/component/form/ChangePasswordForm';
-import { styles } from '@/src/constant/styles.constant';
-import clsx from 'clsx';
+import { MdOutlineSecurity } from 'react-icons/md';
 
 function AccountSetting() {
   return (
     <div className='1400px:px-[15%] px-0 w-full'>
-      <div className='lg:w-[800px] w-full mx-auto'>
-        <div
-          className={clsx(styles.between, 'py-5 border-b border-b-slate-300')}
-        >
-          <div>
-            <h3 className='text-xm font-semibold mb-1'>Login & Security</h3>
-            <p className='opacity-50 font-light text-sm'>
-              Keeping your account secure. We regularly review accounts to make
-              sure they’re secure as possible. We’ll also let you know if
-              there’s more we can do to increase the security of your account.
-            </p>
-          </div>
-        </div>
+      <div className='lg:w-[800px] w-full mx-auto mt-4'>
+        <Alert>
+          <MdOutlineSecurity className='h-5 w-5' />
+          <AlertTitle>Login & Security</AlertTitle>
+          <AlertDescription className='font-light opacity-60 text-sm'>
+            Keeping your account secure. We regularly review accounts to make
+            sure they’re secure as possible. We’ll also let you know if there’s
+            more we can do to increase the security of your account.
+          </AlertDescription>
+        </Alert>
 
         <div>
           <ChangeEmailForm />
