e90bf2cc2b08289a6c28ac18c2f009a2b8c0df67
Z-zenos <tuan_hoang@theinfitech.com>
2025-03-06 18:41:09 +0000

    update: add track user data

--- frontend/src/component/common/Chart/TrackUserActionsChart.tsx
+++ frontend/src/component/common/Chart/TrackUserActionsChart.tsx
@@ -7,22 +7,38 @@ import { ChartContainer, ChartTooltip, ChartTooltipContent } from './Chart';
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from '../Card/Card';
 import {
+  TrackingTimeRangeCode,
   UserActionTypeCode,
   type TrackUserActionsItem,
 } from '@/src/lib/api/generated';
-import { randomHexColor } from '@/src/utils/app.util';
+import { optionify, randomHexColor } from '@/src/utils/app.util';
+import clsx from 'clsx';
+import { styles } from '@/src/constants/styles.constant';
+import { Form, FormCombobox, FormSelect } from '../../form/Form';
+import { useForm } from 'react-hook-form';
 
 interface TrackUserActionsChartProps {
   data: TrackUserActionsItem[];
 }
 
 export function TrackUserActionsChart({ data }: TrackUserActionsChartProps) {
+  const form = useForm<{ action: string[]; time_period: string }>({
+    mode: 'all',
+    defaultValues: {
+      action: [
+        UserActionTypeCode.Bookmark,
+        UserActionTypeCode.ApplyEvent,
+        UserActionTypeCode.PurchaseTicket,
+      ],
+      time_period: TrackingTimeRangeCode.Last7Days,
+    },
+  });
+
   const chartConfig = Object.assign(
     {},
     ...Object.values(UserActionTypeCode).map((uatc) => ({
@@ -35,61 +51,80 @@ export function TrackUserActionsChart({ data }: TrackUserActionsChartProps) {
 
   return (
     <Card className='shadow-md'>
-      <CardHeader>
-        <CardTitle>Line Chart - Multiple</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig}>
-          <LineChart
-            accessibilityLayer
-            data={data}
-            margin={{
-              left: 12,
-              right: 12,
-            }}
-          >
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey='actionAt'
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 3)}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent />}
-            />
-            {[
-              UserActionTypeCode.Bookmark,
-              UserActionTypeCode.ApplyEvent,
-              UserActionTypeCode.PurchaseTicket,
-            ].map((uatc) => (
-              <Line
-                key={uatc}
-                dataKey={`actions.${uatc}`}
-                type='monotone'
-                stroke={chartConfig[uatc].color}
-                strokeWidth={2}
-                dot={false}
+      <Form {...form}>
+        <form>
+          <CardHeader className={clsx(styles.between)}>
+            <CardTitle>Tracking User Actions</CardTitle>
+            <div className={clsx(styles.center, 'gap-3')}>
+              <FormCombobox
+                control={form.control}
+                name='action'
+                options={optionify(UserActionTypeCode)}
+                multiple
+                i18nPath='code.userAction'
               />
-            ))}
-          </LineChart>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter>
-        <div className='flex w-full items-start gap-2 text-sm'>
-          <div className='grid gap-2'>
-            <div className='flex items-center gap-2 font-medium leading-none'>
-              Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
+              <FormSelect
+                control={form.control}
+                name='time_period'
+                options={optionify(TrackingTimeRangeCode)}
+                i18nPath='code.trackingTimeRange'
+              />
             </div>
-            <div className='flex items-center gap-2 leading-none text-muted-foreground'>
-              Showing total visitors for the last 6 months
+          </CardHeader>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <LineChart
+                accessibilityLayer
+                data={data}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
+              >
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey='actionAt'
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent />}
+                />
+                {[
+                  UserActionTypeCode.Bookmark,
+                  UserActionTypeCode.ApplyEvent,
+                  UserActionTypeCode.PurchaseTicket,
+                ].map((uatc) => (
+                  <Line
+                    key={uatc}
+                    dataKey={`actions.${uatc}`}
+                    type='monotone'
+                    stroke={chartConfig[uatc].color}
+                    strokeWidth={2}
+                    dot={false}
+                  />
+                ))}
+              </LineChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter>
+            <div className='flex w-full items-start gap-2 text-sm'>
+              <div className='grid gap-2'>
+                <div className='flex items-center gap-2 font-medium leading-none'>
+                  Trending up by 5.2% this month{' '}
+                  <TrendingUp className='h-4 w-4' />
+                </div>
+                <div className='flex items-center gap-2 leading-none text-muted-foreground'>
+                  Showing total visitors for the last 6 months
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </CardFooter>
+          </CardFooter>
+        </form>
+      </Form>
     </Card>
   );
 }
