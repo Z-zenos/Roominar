8e4713067046808a694768a31d169a5d3be69608
Z-zenos <tuan_hoang@theinfitech.com>
2025-04-02 13:32:03 +0000

    update: format datetime

--- frontend/src/utils/app.util.ts
+++ frontend/src/utils/app.util.ts
@@ -101,7 +101,11 @@ export function toCamelCase(obj: { [key: string]: any }) {
 }
 
 export function formatEventDate(datetime: Date) {
-  return dayjs(datetime).format('YYYY MMM DD - HH:MM');
+  return dayjs(datetime).format(
+    datetime.getFullYear() === new Date().getFullYear()
+      ? 'MMM DD - HH:MM'
+      : 'YYYY MMM DD - HH:MM',
+  );
 }
 
 interface OptionifyOptions {
