ced8937456413d7af37d10208aa1aee4067b9fd7
Z-zenos <tuan_hoang@theinfitech.com>
2025-05-12 03:19:52 +0000

    Feat - Update CORS settings to allow additional origin

--- backend/main.py
+++ backend/main.py
@@ -32,8 +32,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:3001",
         "http://192.168.1.7:3000",
+        "http://192.168.80.62:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
