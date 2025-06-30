ef3cffe3de73a51b93b4672d48ff9ca9a33dd369
Z-zenos <tuan_hoang@theinfitech.com>
2025-06-30 09:58:26 +0000

    fix: update CORS middleware to restrict allowed origins

--- backend/main.py
+++ backend/main.py
@@ -38,12 +38,7 @@ time.tzset()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://192.168.1.6:3000",
-        "http://192.168.80.62:3000",
-        "http://192.168.88.151:3000",
-        "http://192.168.0.111:3000",
-        "http://dungct.navistar.io:2002",
+        "https://vievent.site",
     ],
     allow_credentials=True,
     allow_methods=["*"],
