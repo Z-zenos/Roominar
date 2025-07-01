b108b602b428c60bfdd00641d969ed79e15525cf
Z-zenos <tuan_hoang@theinfitech.com>
2025-07-01 12:30:16 +0000

    fix: correct string interpolation for Stripe publishable key

--- frontend/src/component/common/Payment/ApplicationCheckout.tsx
+++ frontend/src/component/common/Payment/ApplicationCheckout.tsx
@@ -20,7 +20,7 @@ import { handleApiError } from '@/src/utils/app.util';
 // Make sure to call 'loadStripe' outside of a component's render to avoid
 // recreating the 'Stripe' object on every render
 const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  `${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`,
 );
 
 interface ApplicationCheckoutProps {
